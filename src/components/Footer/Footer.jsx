8606f3d5f4956bf47a08849f5584b611dd885945
fathurzoy <fathurrahmanf123@gmail.com>
2021-08-02 02:25:20 +0700

    text color

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
       <div className="container">
         <div className="flex justify-between flex-wrap text-gray-200">
           <div className="w-full sm:w-1/3 px-5 text-left mb-5">
-            <h1 className="text-2xl mb-3">About us</h1>
-            <p className="text-sm">
+            <h1 className="text-2xl mb-3 text-gray-200">About us</h1>
+            <p className="text-sm text-gray-200">
               Alienco photography love to make other people happy about their
               moments and capturing moments in their life. We capture moments
               with pure honesty, true emotion and dedication capture the
@@ -17,8 +17,8 @@ const Footer = () => {
             </p>
           </div>
           <div className="w-full sm:w-1/3 px-5 text-left mb-5">
-            <h1 className="text-2xl mb-3">Our mission</h1>
-            <p className="text-sm">
+            <h1 className="text-2xl mb-3 text-gray-200">Our mission</h1>
+            <p className="text-sm text-gray-200">
               We communicate the story visually, finding something interesting
               in an ordinary place, seen through the artistic eye behind the
               camera lens, capturing your day as it unfolds & creating a truly
@@ -26,22 +26,24 @@ const Footer = () => {
             </p>
           </div>
           <div className="w-full sm:w-1/3 px-5 text-left mb-5">
-            <h1 className="text-2xl mb-3">Our offer</h1>
+            <h1 className="text-2xl mb-3 text-gray-200">Our offer</h1>
             <ul>
               <li className="flex ">
                 <i className="fa fa-dot-circle-o mt-1 pr-2 "></i>
-                <p className="text-sm">
+                <p className="text-sm text-gray-200">
                   Alienco Photography has been officially registered as PT.Satu
                   Wadah Kreasindo
                 </p>
               </li>
               <li className="flex ">
                 <i className="fa fa-dot-circle-o mt-1 pr-2"></i>
-                <p className="text-sm">Fully support and fast respond</p>
+                <p className="text-sm text-gray-200">
+                  Fully support and fast respond
+                </p>
               </li>
               <li className="flex ">
                 <i className="fa fa-dot-circle-o mt-1 pr-2"></i>
-                <p className="text-sm">
+                <p className="text-sm text-gray-200">
                   experience since 2011 ALIENCO has served more than 500
                   couples.
                 </p>
